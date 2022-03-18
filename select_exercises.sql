4711bfd8714b0282cf0c7dc4d4535b4671a10de0
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-18 11:18:10 -0500

    completed select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+SELECT * FROM albums;
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name FROM albums WHERE release_date BETWEEN '1990-01-01' AND '1992-01-01';
+SELECT name FROM albums WHERE sales < 20.0;
+SELECT name FROM albums WHERE genre = 'Rock';
+
+
