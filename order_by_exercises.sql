d3e5a3048a124a9ff37f014f4f3c6d22c7bfdf5d
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-21 11:23:01 -0500

    copied code from where exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,63 @@
+USE employees;
+
+-- PART ONE
+
+
+SELECT *
+FROM employees.departments;
+
+SELECT *
+FROM employees
+WHERE hire_date = 1985;
+
+SELECT employees.first_name
+FROM employees
+WHERE employees.employees.first_name
+          IN ('Irena', 'Vidya', 'Maya');
+
+SELECT employees.last_name
+FROM employees
+WHERE employees.employees.last_name
+          LIKE 'E%';
+
+SELECT employees.last_name
+FROM employees
+WHERE employees.employees.last_name
+          LIKE '%q%';
+
+-- PART TWO
+-- 1.
+SELECT employees.first_name
+FROM employees
+WHERE employees.employees.first_name = 'Irena'
+   OR employees.employees.first_name = 'Vidya'
+   OR employees.employees.first_name = 'Maya';
+
+SELECT *
+FROM employees;
+
+-- 2.
+SELECT employees.first_name
+FROM employees
+WHERE (employees.employees.first_name = 'Irena'
+    OR employees.employees.first_name = 'Vidya'
+    OR employees.employees.first_name = 'Maya')
+  AND employees.employees.gender = 'M';
+
+-- 3.
+SELECT employees.last_name
+FROM employees
+WHERE employees.employees.last_name LIKE 'E%'
+   OR employees.employees.last_name LIKE '%E';
+
+-- 4.
+SELECT employees.last_name
+FROM employees
+WHERE employees.employees.last_name LIKE 'E%'
+  AND employees.employees.last_name LIKE '%E';
+
+-- 5.
+SELECT employees.last_name
+FROM employees
+WHERE employees.employees.last_name LIKE '%q%'
+  AND employees.employees.last_name NOT LIKE '%qu%';
