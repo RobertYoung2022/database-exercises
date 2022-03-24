224ce10201130a254f977aa201703f19080ab5a5
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-24 10:03:56 -0500

    created join test db tables

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,2 +1,62 @@
-USE employees;
+-- USE employees;
+
+
+
+
+CREATE DATABASE IF NOT EXISTS join_test_db;
+-- SHOW DATABASES;
+
+USE join_test_db;
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users
+(
+    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(100) NOT NULL,
+    email   VARCHAR(100) NOT NULL,
+    role_id INT UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (id)
+    -- FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name)
+VALUES ('admin');
+
+INSERT INTO roles (name)
+VALUES ('author');
+
+INSERT INTO roles (name)
+VALUES ('reviewer');
+
+INSERT INTO roles (name)
+VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id)
+VALUES ('bob', 'bob@example.com', 1),
+       ('joe', 'joe@example.com', 2),
+       ('sally', 'sally@example.com', 3),
+       ('adam', 'adam@example.com', 3),
+       ('jane', 'jane@example.com', null),
+       ('mike', 'mike@example.com', null),
+       ('bobert', 'bobert@example.com', null),
+       ('shae', 'shae@example.com', 2),
+       ('kim', 'kim@example.com', 2),
+       ('jon', 'jon@example.com', 2);
+
+DESCRIBE roles;
+DESCRIBE users;
+
+
+SELECT * FROM users;
+
+
+
 
